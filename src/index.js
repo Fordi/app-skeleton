98ad547d9140ae38010cb46ba005876a080060f9
Bryan Elliott <bryan_elliott@comcast.com>
2022-03-29 10:21:10 -0400

    Corrected commentary

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ render(html`
       '/test/:id/details',
       // -> /pages/test/details.js { id: ..., name: ... }, supports `/` in `name`
       '/test/:id/details/*name',
-      // -> /pages/test/details.js { id: ... }, supports `/` in `id`
+      // -> /pages/test/index.js { id: ... }, supports `/` in `id`
       '/test/*id/',
     ]}
   />
